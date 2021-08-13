69385322c690b982e9b750cd0aa638d7b7b907c2
abby <2016644182@qq.com>
2021-08-13 16:31:12 +0800

    '$parent使用$on和$emit'

---
+++ src/components/child.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="sayHi">say hi</button>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    // 监听事件
+    this.$parent.$on('foo', msg => {
+      console.log(msg)
+    })
+  },
+  methods: {
+    sayHi () {
+      // 派发着和监听者是同一个
+      this.$parent.$emit('foo', '执行了')
+    }
+  }
+}
+</script>
---
+++ src/views/father.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Child />
+    <Child />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Child from '@/components/child.vue'
+
+export default {
+  name: 'child',
+  components: {
+    Child
+  }
+}
+</script>
