ff1cd2e45ef52ea4ac537360a15a8f510d34eb94
abby <2016644182@qq.com>
2021-08-16 20:43:23 +0800

    用Vue.extend优化实现类似与element里面的$message类似效果

--- src/utils/create.js
+++ src/utils/create.js
@@ -1,23 +1,17 @@
 import Vue from "vue";
 // 创建函数接收要创建组件定义
 function create(Component, props) {
-  debugger
+  // 组件构造函数获取
+  const Ctor = Vue.extend(Component)
+  // 获取组件实例
+  const comp = new Ctor({propsData: props})
+  comp.$mount()
 
-// 创建⼀个Vue新实例
-  const vm = new Vue({
-    render(h) {
-      // render函数将传⼊组件配置对象转换为虚拟dom
-      return h(Component, { props });
-    }
-  }).$mount(); //执⾏挂载函数，但未指定挂载⽬标，表示只执⾏初始化⼯作
-  // 将 ⽣ 成 dom 元 素 追 加 ⾄ body
-   document.body.appendChild(vm.$el);
+  document.body.appendChild(comp.$el);
 
-  // 给组件实例添加销毁⽅法
-  const comp = vm.$children[0];
   comp.remove = () => {
-    document.body.removeChild(vm.$el);
-    vm.$destroy();
+    document.body.removeChild(comp.$el);
+    comp.$destroy();
   };
   return comp;
 }
